00e9bd5f4cac32ed44c9bad896ea649c55e91286
刘 理想 <liulixiang@liudeMacBook-Air.local>
2012-10-28 20:56:53 +0800

    get the udacity course list
    获取udacity的课程列表

---
+++ webcrawler.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+def get_page(url):
+    try:
+        import urllib
+        return urllib.urlopen(url).read()
+    except:
+        return ''
+
+
+def get_all_links(page):
+    try:
+        import re
+        filelinks_pat = re.compile('href[\s]*=[\s]*"([^"]*?zip)"')
+        links = filelinks_pat.findall(page)
+        return links
+    except:
+        return []
+
+
+def store_links(url, file_name):
+    links = get_all_links(get_page(url))
+    #print links
+    try:
+        import codecs
+        f = codecs.open(u'downloadlist_%s.txt' % file_name, 'w', 'utf-8')
+        f.write(u'downloadlist_%s\n' % file_name)
+        for link in links:
+            link = link.replace(u'&amp;', u'&')
+            f.write('%s\n' % link)
+        f.close()
+        print u'下载列表保存完毕\n'
+    except Exception as e:
+        print u'无法保存下载链接\n%s' % e
+
+
+def main(url, file_name):
+    store_links(url, file_name)
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) != 3:
+        print u'请输入url与要保存的课程名称'
+    else:
+        main(sys.argv[1], sys.argv[2])
