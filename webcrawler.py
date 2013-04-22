b5d85791870e5479ef12b10c48c91c7f596daef6
Liu Lixiang <liulixiang1988@gmail.com>
2013-04-22 21:38:28 +0800

    添加了一些课程的下载列表

--- webcrawler.py
+++ webcrawler.py
@@ -13,9 +13,10 @@ def get_page(url):
 def get_all_links(page):
     try:
         import re
-        filelinks_pat = re.compile('href[\s]*=[\s]*"([^"]*?zip)"')
+        filelinks_pat = re.compile('href[\s]*=[\s]*"([^"]*?zip|[^"]*?rar|[^"]*?pdf)"')
         links = filelinks_pat.findall(page)
-        return links
+        #按后缀名排序
+        return sorted(links, cmp=lambda x, y: 1 if x[-3:] > y[-3:] else -1)
     except:
         return []
 
@@ -28,13 +29,19 @@ def store_links(url, file_name):
         f = codecs.open(u'downloadlist_%s.txt' % file_name, 'w', 'utf-8')
         f.write(u'downloadlist_%s\n' % file_name)
         for link in links:
-            link = link.replace(u'&amp;', u'&')
+            link = verify_link(url, link)
             f.write('%s\n' % link)
         f.close()
         print u'下载列表保存完毕\n'
     except Exception as e:
         print u'无法保存下载链接\n%s' % e
 
+def verify_link(url, link):
+    link = link.replace(u'&amp;', u'&')
+    if link.startswith('http'):
+        return link
+    else:
+        return url[:url.rfind('/')]+link
 
 def main(url, file_name):
     store_links(url, file_name)
